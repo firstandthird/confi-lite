44f7c463e3ca8655cd9e63456fa61d270aaa9901
orthagonal <orthagonal1@gmail.com>
2019-05-28 01:18:20 +0000

    feedback

--- index.js
+++ index.js
@@ -3,27 +3,18 @@ const fs = require('fs');
 const aug = require('aug');
 const varson = require('varson');
 
-module.exports = (path = '@architect/shared/conf') => {
+const loadConfig = (fileName) => {
+  if (fs.existsSync(fileName)) {
+    return JSON.parse(fs.readFileSync(fileName));
+  }
+};
+module.exports = (path = `${__dirname}/conf`) => {
   const env = process.env.NODE_ENV || 'dev';
   const envFile = `${path}/${env}.json`;
   const defaultFile = `${path}/default.json`;
-  let envConfig = {};
   console.log(`Loading config from ${envFile} and ${defaultFile}`);
-  if (fs.existsSync(envFile)) {
-    try {
-      envConfig = JSON.parse(fs.readFileSync(envFile));
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  let defaultConfig = {};
-  if (fs.existsSync(defaultFile)) {
-    try {
-      defaultConfig = JSON.parse(fs.readFileSync(defaultFile));
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  let envConfig = loadConfig(envFile);
+  let defaultConfig = loadConfig(defaultFile);
   const config = aug(defaultConfig, envConfig);
   return varson(config, helpers);
 };
