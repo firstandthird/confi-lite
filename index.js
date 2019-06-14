bf7cfb9d4f949d59d7e731630aa33f3db232fa60
orthagonal <orthagonal1@gmail.com>
2019-06-14 18:53:42 +0000

    baseConfig

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const loadConfig = (fileName) => {
   }
   return {};
 };
-module.exports = (path = `${__dirname}/conf`, env = process.env.NODE_ENV || 'dev') => {
+module.exports = (path = `${__dirname}/conf`, env = process.env.NODE_ENV || 'dev', baseConfig = {}) => {
   const envFile = `${path}/${env}.json`;
   const defaultFile = `${path}/default.json`;
   let envConfig = loadConfig(envFile);
   let defaultConfig = loadConfig(defaultFile);
-  const config = aug(defaultConfig, envConfig);
+  const config = aug(baseConfig, defaultConfig, envConfig);
   config.env = process.env;
   return varson(config, helpers);
 };
